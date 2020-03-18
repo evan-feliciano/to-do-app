4f3257eec605e43622793d95234d2db39a6703c9
Evan Feliciano <evanf@Evans-MacBook-Pro.local>
2020-03-18 10:59:15 -0400

    checkpoint-12.13-DOM-Scripting-Pt2: completed

---
+++ app.js
@@ -0,0 +1,57 @@
+function onReady() {
+  let toDos = [];
+  let id = 0;
+
+  const addToDoForm = document.getElementById('addToDoForm');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+      toDos.push({
+        title: newToDoText.value,
+        complete: false
+        id:id
+      });
+
+      id++;
+
+      newToDoText.value = '';
+
+      renderTheUI();
+  }
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete!";
+
+    deleteButton.addEventListener('click', event => {
+      alert(toDo.title);
+      renderTheUI();
+    })
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+    });
+  }
+  addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
+  });
+  renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
+};
